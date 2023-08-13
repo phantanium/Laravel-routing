b5488bb94975fa7336e8cd2e5d7481a88c2f4cae
Khaliq Fahul <naufalfatihul36@gmail.com>
2023-08-13 09:05:07 +0700

    first and completed commit

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/contacts', function() {
+    return "<h1> Daftar Kontak</h1>";
+ });
+
+Route::get('/contacts/create', function() {
+    return "<h1>Tambah Kontak Baru</h1>";
+ });
+
+ Route::get('/contacts/{id}', function($id) {
+    return "Ini Kontak ke-" . $id;
+ }); 
+ 
+Route::get('/contacts/{id}', function($id) {
+    return "Ini Kontak ke-" . $id;
+ })->whereNumber('id');
+ Route::get('/companies/{name?}', function($name=null) {
+    if($name) {
+        return "Nama Perusahaan: " . $name;
+    } else {
+        return "Nama Perusahaan Kosong";
+    }
+ })->whereAlphaNumeric('name');
 
 Route::get('/', function () {
     $html = "
