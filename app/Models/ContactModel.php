7d49c24f8792e61f658c1703502d5f5ae88f6a08
Khaliq Fahul <naufalfatihul36@gmail.com>
2023-09-03 01:51:38 +0700

    working well :)

---
+++ app/Models/ContactModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactModel extends Model
+{
+    use HasFactory;
+    protected $table = 'contacts';
+    protected $fillable = ['user_id', 'name', 'phone_number', 'avatar', 'gender'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,69 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function() {
-    return "<h1> Daftar Kontak</h1>";
- });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/contacts/create', function() {
-    return "<h1>Tambah Kontak Baru</h1>";
- });
-
- Route::get('/contacts/{id}', function($id) {
-    return "Ini Kontak ke-" . $id;
- }); 
- 
-Route::get('/contacts/{id}', function($id) {
-    return "Ini Kontak ke-" . $id;
- })->whereNumber('id');
- Route::get('/companies/{name?}', function($name=null) {
-    if($name) {
-        return "Nama Perusahaan: " . $name;
-    } else {
-        return "Nama Perusahaan Kosong";
-    }
- })->whereAlphaNumeric('name');
-
-Route::get('/', function () {
-    $html = "
-    <h1>Contact App</h1>
-    <div>
-        <a href='" . route('admin.contacts.index') . "'>All contacts</>
-        <a href='" . route('admin.contacts.create') . "'>Add contacts</>
-        <a href='" . route('admin.contacts.show', 1) . "'>Show contacts</>
-    </div>
-    ";
-    return $html;
-    // return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('contacts', ContactController::class);
 });
 
-Route::prefix('admin')->name('admin.')->group(function() {
-    Route::get('/contacts', function() {
-        return "<h1>Daftar Kontak</h1>";
-    })->name('contacts.index');
-    
-    Route::get('/contacts/create', function() {
-        return "<h1>Tambah Kontak Baru</h1>";
-    })->name('contacts.create');
-    
-    Route::get('/contacts/{id}', function($id) {
-        return "Ini Kontak ke-" . $id;
-    })->whereNumber('id')->name('contacts.show');
-    
-    Route::get('/companies/{name?}', function($name=null) {
-        if($name) {
-            return "Nama Perusahaan: " . $name;
-        } else {
-            return "Nama Perusahaan Kosong";
-        }
-    })->whereAlphaNumeric('name')->name('companies');
-});
-
-Route::fallback(function() {
-    return "<h1>Maaf, halaman yang anda tuju tidak ada</h1>";
-});
-
+require __DIR__.'/auth.php';
